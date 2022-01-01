a2345ebefd75345040178f0674cb5345e466d842
Devrath <devrath.dev595@gmail.com>
2022-01-01 12:16:27 +0530

    v

--- Applications/Snippets/app/src/main/java/com/example/code/demos/statesSamples/updateTimer/UpdateTimer.kt
+++ Applications/Snippets/app/src/main/java/com/example/code/demos/statesSamples/updateTimer/UpdateTimer.kt
@@ -3,36 +3,72 @@ package com.example.code.demos.statesSamples.updateTimer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.code.demos.theme.CodeTheme
+import kotlin.concurrent.timer
 
 class UpdateTimer : ComponentActivity() {
+
+    private var currentValue by mutableStateOf(0)
+    private var tikTokTimer = timer(period = 50){ currentValue ++ }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CodeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            CurrentScreen()
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+    override fun onDestroy() {
+        super.onDestroy()
+        tikTokTimer.cancel()
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview2() {
-    CodeTheme {
-        Greeting("Android")
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultPreview() {
+        CurrentScreen()
+    }
+
+    @Composable
+    private fun CurrentScreen() {
+        CodeTheme {
+            // A surface container using the 'background' color from the theme
+            Surface(color = MaterialTheme.colors.background) {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(
+                            text = currentValue.toString(),
+                            style = TextStyle(
+                                color = Color.Black,
+                                fontSize = 25.sp
+                            ),
+                            modifier = Modifier.fillMaxWidth(),
+                            textAlign = TextAlign.Center
+                        )
+                    }
+            }
+        }
     }
 }
