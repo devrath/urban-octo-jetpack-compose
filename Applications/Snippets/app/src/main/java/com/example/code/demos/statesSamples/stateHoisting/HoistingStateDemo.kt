628982d2af84792599112b4952e09750f8b36baf
Devrath <devrath.dev595@gmail.com>
2022-01-02 10:51:34 +0530

    v

--- Applications/Snippets/app/src/main/java/com/example/code/demos/statesSamples/stateHoisting/HoistingStateDemo.kt
+++ Applications/Snippets/app/src/main/java/com/example/code/demos/statesSamples/stateHoisting/HoistingStateDemo.kt
@@ -3,36 +3,72 @@ package com.example.code.demos.statesSamples.stateHoisting
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.code.ui.theme.CodeTheme
 
 class HoistingStateDemo : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            CodeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+        setContent { CurrentScreen() }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultPreview() { CurrentScreen() }
+
+    @Composable
+    private fun CurrentScreen() {
+
+        var textState by remember{ mutableStateOf("Initial Text State") }
+
+        CodeTheme {
+            // A surface container using the 'background' color from the theme
+            Surface(
+                color = MaterialTheme.colors.background,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.Magenta),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+
+                    Text(
+                        text = textState,
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center,
+                        style = TextStyle(
+                            fontSize = 24.sp, fontWeight = FontWeight.Bold
+                        )
+                    )
+
+                    Button(
+                        onClick = { textState = "Final Text State" },
+                        modifier = Modifier.wrapContentSize()
+                    ) {
+                        Text(text = "Click to change the text")
+                    }
+
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview2() {
-    CodeTheme {
-        Greeting("Android")
-    }
 }
