3c9ae47246b6c030d732f430d1750f56c86122bc
Devrath <devrath.dev595@gmail.com>
2022-01-16 22:02:23 +0530

    v

--- Applications/Snippets/app/src/main/java/com/example/code/demos/widgetSamples/BoxComposableDemo.kt
+++ Applications/Snippets/app/src/main/java/com/example/code/demos/widgetSamples/BoxComposableDemo.kt
@@ -3,9 +3,18 @@ package com.example.code.demos.widgetSamples
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.BiasAlignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.code.ui.theme.CodeTheme
 
@@ -31,6 +40,17 @@ private fun CurrentScreen() {
     CodeTheme {
         // A surface container using the 'background' color from the theme
         Surface(color = MaterialTheme.colors.background) {
+
+            Box(
+                Modifier
+                    .background(Color.Gray)
+                    .fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+
+                Text(text = "Hello")
+
+            }
 
         }
     }
