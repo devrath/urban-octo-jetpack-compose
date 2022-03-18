af77db550ef2d884e4bb0d1fd47eb9161a910cdf
Devrath <devrath@mplgaming.com>
2022-03-18 20:14:45 +0530

    initial code

---
+++ Applications/TrackerApp/app/src/main/java/com/demo/tracker/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.demo.tracker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.demo.tracker.ui.theme.CaloryTrackerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CaloryTrackerTheme {
+
+            }
+        }
+    }
+}
