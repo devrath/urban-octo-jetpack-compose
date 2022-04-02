e38d1c6441969de7d50df1c0f1c6be46bcfce6ae
Devrath <devrath@mplgaming.com>
2022-04-02 18:02:39 +0530

    v

--- Applications/Snippets/app/src/main/java/com/example/code/demos/statesSamples/viewModelDemo/ViewModelDemo.kt
+++ Applications/Snippets/app/src/main/java/com/example/code/demos/statesSamples/viewModelDemo/ViewModelDemo.kt
@@ -6,83 +6,58 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
-import com.example.code.ui.theme.CodeTheme
+
 
 class ViewModelDemo : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { CurrentScreen() }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CurrentScreen()
-}
-
-@Composable
-private fun CurrentScreen() {
-
-    val currentText = remember { mutableStateOf("") }
-
-    CodeTheme {
-        Surface(color = MaterialTheme.colors.background) {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
-            ) {
-                DisplayText("")
-                InputField(newValueStr = currentText.value) { currentText.value = it }
-                ActionButton {
-
-                }
-            }
+        setContent {
+            MainScreen()
         }
     }
 }
 
 @Composable
-private fun ActionButton(clickAction : () -> Unit) {
-    Button(
-        onClick = clickAction,
-        modifier = Modifier.wrapContentSize(),
-        
+fun MainScreen(viewModel: ViewModelDemoVm = ViewModelDemoVm()) {
+    val newNameStateContent = viewModel.textFieldState.observeAsState("Initial State")
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Click To Update Text")
+        GreetingMessage(newNameStateContent.value) {
+                // New value to be populated
+                viewModel.onTextChanged("Final Value")
+        }
     }
 }
 
 @Composable
-private fun InputField(
-    newValueStr:String,
-    inputUiUpdate:(newValue:String) -> Unit
+fun GreetingMessage(
+    valueDisplayed: String,
+    textFieldUpdate: () -> Unit
 ) {
-    TextField(value = newValueStr, onValueChange = inputUiUpdate)
+
+    Text(text = valueDisplayed)
+
+    Button(onClick = textFieldUpdate ) {
+        Text(text = "Click to execute !")
+    }
 }
 
+
+@Preview(showBackground = true)
 @Composable
-private fun DisplayText(displayStr:String) {
-    Text(
-        text = displayStr,
-        modifier = Modifier.wrapContentSize(),
-        style = TextStyle(
-            fontSize = 24.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color.Magenta
-        ),
-        textAlign = TextAlign.Center
-    )
+fun DefaultPreview() {
+    MainScreen()
 }
--- Applications/Snippets/app/src/main/java/com/example/code/demos/statesSamples/viewModelDemo/ViewModelDemoVm.kt
+++ Applications/Snippets/app/src/main/java/com/example/code/demos/statesSamples/viewModelDemo/ViewModelDemoVm.kt
@@ -1,6 +1,14 @@
 package com.example.code.demos.statesSamples.viewModelDemo
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ViewModelDemoVm : ViewModel() {
+
+    val textFieldState = MutableLiveData("Initial State")
+
+    fun onTextChanged(newTxt : String) {
+        textFieldState.value = newTxt
+    }
+
 }
