e52ccea66c7f71e8d784ae21b4cc04d6ca24795b
Devrath <devrath@mplgaming.com>
2023-01-13 22:35:28 +0530

    removed unused project

--- Applications/TrackerApp/app/src/main/java/com/demo/tracker/CaloreTrackerApp.kt
+++
@@ -1,8 +0,0 @@
-package com.demo.tracker
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class CaloreTrackerApp : Application() {
-}
--- Applications/TrackerApp/core-ui/build.gradle.kts
+++
@@ -1,7 +0,0 @@
-apply {
-    from("$rootDir/compose-module.gradle")
-}
-
-dependencies {
-
-}
