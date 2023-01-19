e7afa81006e42c6540944a5b6e0a07f5977d78a9
Devrath <devrath@mplgaming.com>
2023-01-19 22:54:06 +0530

    new code added

---
+++ Applications/Application/app/src/main/java/com/istudio/code/ImageCardActivity.kt
@@ -0,0 +1,105 @@
+package com.istudio.code
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+class ImageCardActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val painter = painterResource(id = R.drawable.grogu)
+            val contentDescription = "Grogu says hi"
+            val title = "Force is strong with Grogu"
+
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Box(
+                    modifier = Modifier.fillMaxWidth(0.5f)
+                ) {
+                    ImageCard(
+                        title = title,
+                        contentDescription = contentDescription,
+                        painter = painter
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ImageCard(
+    title: String,
+    contentDescription:String,
+    painter: Painter,
+    modifier:Modifier=Modifier
+){
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(18.dp),
+        elevation = 5.dp
+    ) {
+        Box(
+            modifier = Modifier.height(200.dp)
+        ) {
+            // Image
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
+            // Gradient
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent, Color.Black
+                            ),
+                            startY = 300f
+                        )
+                    )
+            )
+            // Title
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ) {
+                Text(
+                    text = title,
+                    style = TextStyle(color = Color.White, fontSize = 12.sp)
+                )
+            }
+        }
+    }
+}
