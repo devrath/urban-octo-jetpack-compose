2ed47a3139529684ab4972a3602f9d2c9bf4ae35
Devrath <devrath@mplgaming.com>
2023-01-20 12:57:37 +0530

    image added

---
+++ Applications/Application/app/src/main/java/com/istudio/code/demos/state/StateActivity.kt
@@ -0,0 +1,84 @@
+package com.istudio.code.demos.state
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+class StateActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Column(
+                modifier = Modifier
+                    .background(Color.DarkGray)
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                ColorBox()
+            }
+        }
+    }
+}
+
+@Composable
+fun ColorBox(modifier: Modifier = Modifier) {
+    val counter: MutableState<Int> = remember { mutableStateOf(1) }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = counter.value.toString(),
+            fontSize = 40.sp,
+            color = Color.White
+        )
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp)
+        )
+        Box(
+            modifier = Modifier
+                .clip(RoundedCornerShape(18.dp))
+                .background(Color.Yellow)
+                .width(100.dp)
+                .height(100.dp)
+                .clickable {
+                    counter.value++
+                },
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = "+",
+                textAlign = TextAlign.Center,
+                color = Color.Magenta,
+                fontSize = 40.sp
+            )
+        }
+    }
+}
