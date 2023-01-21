d1ab43662b47c1e91dbde1ec897c7a2c5e3fe63b
Devrath <devrath.dev595@gmail.com>
2023-01-21 12:09:47 +0530

    image added

--- Applications/Application/app/src/main/java/com/istudio/code/demos/constraintLayout/ConstraintLayoutActivity.kt
+++ Applications/Application/app/src/main/java/com/istudio/code/demos/constraintLayout/ConstraintLayoutActivity.kt
@@ -3,11 +3,60 @@ package com.istudio.code.demos.constraintLayout
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
 
 class ConstraintLayoutActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+
+            val constraints = ConstraintSet {
+                val greenBox = createRefFor("greenBoxId")
+                val redBox = createRefFor("redBoxId")
+
+                constrain(greenBox) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                    width = Dimension.value(100.dp)
+                    height = Dimension.value(100.dp)
+                }
+
+                constrain(redBox) {
+                    top.linkTo(parent.top)
+                    start.linkTo(greenBox.end)
+                    width = Dimension.value(100.dp)
+                    height = Dimension.value(100.dp)
+                }
+            }
+
+            ConstraintLayout(
+                constraints,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Box(
+                    modifier = Modifier
+                        .background(Color.Green)
+                        .layoutId("greenBoxId")
+                ) {
+
+                }
+                Box(
+                    modifier = Modifier
+                        .background(Color.Red)
+                        .layoutId("redBoxId")
+                ) {
+
+                }
+            }
 
         }
     }
