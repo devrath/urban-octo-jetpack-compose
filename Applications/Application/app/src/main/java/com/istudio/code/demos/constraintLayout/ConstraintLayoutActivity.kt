40375bb91d4e058b18c98e91a345b60709049229
Devrath <devrath.dev595@gmail.com>
2023-01-21 11:24:31 +0530

    gradle dependency added

---
+++ Applications/Application/app/src/main/java/com/istudio/code/demos/constraintLayout/ConstraintLayoutActivity.kt
@@ -0,0 +1,14 @@
+package com.istudio.code.demos.constraintLayout
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class ConstraintLayoutActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
