e4db55f25b33f89391ff5f21c0602adff91c36f2
Devrath <devrath.dev595@gmail.com>
2023-01-23 13:22:10 +0530

    reformatted code

---
+++ Applications/Application/app/src/main/java/com/istudio/code/demos/sideeffect/demos/derivedStateOf/DerivedStateOfActivity.kt
@@ -0,0 +1,13 @@
+package com.istudio.code.demos.sideeffect.demos.derivedStateOf
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class DerivedStateOfActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {}
+    }
+}
